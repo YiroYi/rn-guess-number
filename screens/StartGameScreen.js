69cb9e115639ca7ad69564a1b3f1f28cac9c8401
YiroYi <yiroyi@live.com>
2020-12-06 20:38:38 -0600

    Adding Buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput} from 'react-native';
 
 const StartGameScreen = props => {
   return(
     <View style={styles.screen}>
-      <Text>The game screen</Text>
+      <Text style={styles.title}>Start a New Game!</Text>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Confirm" onPress={() => {}} />
+          </View>
+        </View>
     </View>
   );
 }
@@ -14,7 +24,25 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: 'center',
-    backgroundColor: 'gray'
+    backgroundColor: 'red'
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15
+  },
+  button: {
+    width: 100
   }
 });
 
