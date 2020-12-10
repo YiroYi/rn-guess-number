f3e00c4a49550b2e7805402eb8d0ba5b3346a8e6
YiroYi <yiroyi@live.com>
2020-12-10 00:10:01 -0600

    Adding Gameover screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
@@ -18,13 +18,23 @@ const generateRandomBetween = (min, max, exclude) => {
 const GameScreen = props => {
   const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
 
+  const [rounds, setRounds] = useState(0);
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
+
+  const { userChoice, onGameOver } = props;
+
+  useEffect(() => {
+    if(currentGuess === userChoice) {
+      onGameOver(rounds);
+    }
+  },[currentGuess, userChoice, onGameOver]); //everytime component is rendered this function runs.
 
   const nextGuessHandler = direction => {
     if((direction === 'lower' && currentGuess < props.userChoice) || (direction === 'greater' && currentGuess > props.userChoice)) {
       Alert.alert('Don\'t lie', 'You know that is wrong...',
                   [
+
                     {
                       text: 'Sorry',
                       style: 'cancel'
@@ -41,6 +51,7 @@ const GameScreen = props => {
 
     const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
     setCurrentGuess(nextNumber);
+    setRounds(curRounds => curRounds + 1 );
   }
 
   return(
